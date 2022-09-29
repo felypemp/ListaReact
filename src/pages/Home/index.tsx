11121d0ffcc27bb5c4212af5332884b87679b27e
Felipe <fmp.ti@outlook.com>
2022-09-29 11:11:07 -0300

    Criando os filtros de pesquisa por nome e nacionalidade

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
 import { Header } from '../../components/Header';
 import { 
     Container, 
@@ -11,6 +12,11 @@ import { UserData } from '../../types';
 
 export function Home(){
     const [dataFetching, setDataFetching] = useState<UserData[]>([]);
+    const [dataFetchingBackup, setDataFetchingBackup] = useState<UserData[]>([]);
+    const [isFetching, setIsFetching] = useState(true);
+    const [error, setError] = useState(null);
+    const [ search, setSearch] = useState('');
+    const [searchCountry, setSearchCountry] = useState('');
 
     useEffect(() => {
         axios.get('https://randomuser.me/api/', {
@@ -20,54 +26,103 @@ export function Home(){
         })
         .then(response => {
             setDataFetching(response.data.results);
+            setDataFetchingBackup(response.data.results);
         })
         .catch(error => {
-            console.log(error);
+            setError(error);
+        })
+        .finally(() => {
+            setIsFetching(false);
         })
     }, [] )
+
+    useEffect(() => {
+        if(search.length !==0){
+            const filter = dataFetching.filter(a =>
+                a.name.first.toLocaleUpperCase().indexOf(search.toUpperCase()) >=0 || a.name.last.toLocaleUpperCase().indexOf(search.toUpperCase()) >=0
+            );
+
+            setDataFetching(filter);
+        } else {
+            setDataFetching(dataFetchingBackup);
+        }
+    },[search]);
+
+    useEffect(( ) => {
+        if(searchCountry !== ''){
+            const filterCountry = dataFetching.filter( a => a.location.country.toUpperCase().indexOf(searchCountry.toUpperCase()));
+            setDataFetching(filterCountry);
+        } else{
+            setDataFetching(dataFetchingBackup);
+        }
+    }, [searchCountry])
+
     return (
         <Container>
             <Header/>
             <Content>
                 <h1>Lista dos alunos do curso</h1>
-                <FilterForm>
-                    <div>
-                        <label htmlFor="">Pesquisar</label>
-                        <input type="text" id="nome" placeholder="Nome do aluno"/>
-                    </div>
-                    <div>
-                        <label htmlFor="nacionalidade">Nacionalidade</label>
-                        <select name="" id="nacionalidade">
-                            <option value="">Todas</option>
-                        </select>
-                    </div>
-                </FilterForm>
-                <TableContent>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Sexo</th>
-                            <th>Nacionalidade</th>
-                            <th>Ação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            dataFetching.map((e, index) => {
-                                return(
-                                    <tr key={index}>
-                                        <td>{`${e.name.first} ${e.name.last}`}</td>
-                                        <td>{e.gender}</td>
-                                        <td>{e.nat}</td>
-                                        <td>
-                                            <button>Visualizar</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </TableContent>
+                {
+                    isFetching ? (
+                        <ReactLoading type="spin" color="#FFF" />
+                    ): (
+                        <>
+                        <FilterForm>
+                        <div>
+                            <label htmlFor="">Pesquisar</label>
+                            <input type="text" id="nome" placeholder="Nome do aluno" onChange={(e) => setSearch(e.target.value)}/>
+                        </div>
+                        <div>
+                            <label htmlFor="nacionalidade">Nacionalidade</label>
+                            <select 
+                                name="nacionalidade" 
+                                id="nacionalidade" 
+                                onChange={(e) => setSearchCountry(e.target.value)}
+                            
+                            >
+                                <option value="">Todas</option>
+                                {
+                                    dataFetching.map((e, index) => {
+                                        return (
+                                            <option key={index} value={e.location.country}>
+                                                {e.location.country}
+                                            </option>
+                                        )
+                                    })
+                                }
+                            </select>
+                        </div>
+                    </FilterForm>
+                    <TableContent>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Sexo</th>
+                                <th>Nacionalidade</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                dataFetching.map((e, index) => {
+                                    return(
+                                        <tr key={index}>
+                                            <td>{`${e.name.first} ${e.name.last}`}</td>
+                                            <td>{e.gender}</td>
+                                            <td>{e.nat}</td>
+                                            <td>
+                                                <button>Visualizar</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </TableContent>
+                        </>
+                    )
+                }
+
             </Content>
         </Container>
     )
