7d66aa1bd710ce2b522cd5f0b6c023f6ed4d36d5
Felipe <fmp.ti@outlook.com>
2022-10-04 16:39:25 -0300

    Criando modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,31 +2,42 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import ReactLoading from 'react-loading';
 import { Header } from '../../components/Header';
-import { 
-    Container, 
-    Content, 
+import {
+    Container,
+    Content,
     FilterForm,
-    TableContent,
+    TableContent
 } from './styles';
 import { UserData } from '../../types';
+import { ModalInfo } from '../../components/ModalInfo';
 
 export function Home(){
     const [dataFetching, setDataFetching] = useState<UserData[]>([]);
-    const [dataFetchingBackup, setDataFetchingBackup] = useState<UserData[]>([]);
     const [isFetching, setIsFetching] = useState(true);
     const [error, setError] = useState(null);
-    const [ search, setSearch] = useState('');
+    const [search, setSearch] = useState('');
+    const [isModalInfoOpen, setIsModalInfoOpen] = useState(false);
+    const [dataUser, setdataUser] = useState<UserData>();
     const [searchCountry, setSearchCountry] = useState('');
+
+
+    function handleOpenModalInfo(userSelected: UserData){
+        setdataUser(userSelected);
+        setIsModalInfoOpen(true);
+    }
+
+    function handleCloseModalInfo() {
+        setIsModalInfoOpen(false);
+    }
 
     useEffect(() => {
         axios.get('https://randomuser.me/api/', {
             params: {
-                results:10
+                results: 10
             }
         })
         .then(response => {
             setDataFetching(response.data.results);
-            setDataFetchingBackup(response.data.results);
         })
         .catch(error => {
             setError(error);
@@ -34,96 +45,107 @@ export function Home(){
         .finally(() => {
             setIsFetching(false);
         })
-    }, [] )
+    },[]);
 
-    useEffect(() => {
-        if(search.length !==0){
-            const filter = dataFetching.filter(a =>
-                a.name.first.toLocaleUpperCase().indexOf(search.toUpperCase()) >=0 || a.name.last.toLocaleUpperCase().indexOf(search.toUpperCase()) >=0
-            );
+    const filteredCountry = searchCountry.length > 0
+        ? dataFetching.filter(a => a.location.country.toUpperCase().includes(searchCountry.toUpperCase()))
+        : [];
 
-            setDataFetching(filter);
-        } else {
-            setDataFetching(dataFetchingBackup);
-        }
-    },[search]);
+    const filteredName = search.length > 0 
+        ? dataFetching.filter(a => (a.name.first.toUpperCase() || a.name.last.toUpperCase()).includes(search.toUpperCase()))
+        : [];
 
-    useEffect(( ) => {
-        if(searchCountry !== ''){
-            const filterCountry = dataFetching.filter( a => a.location.country.toUpperCase().indexOf(searchCountry.toUpperCase()));
-            setDataFetching(filterCountry);
-        } else{
-            setDataFetching(dataFetchingBackup);
-        }
-    }, [searchCountry])
+    const actuallyFilter = search.length > 0 ? filteredName : filteredCountry;
 
     return (
         <Container>
-            <Header/>
+            <Header />
             <Content>
                 <h1>Lista dos alunos do curso</h1>
                 {
                     isFetching ? (
-                        <ReactLoading type="spin" color="#FFF" />
-                    ): (
+                        <ReactLoading type="spin" color="#FFF" /> 
+                    ):(
                         <>
-                        <FilterForm>
-                        <div>
-                            <label htmlFor="">Pesquisar</label>
-                            <input type="text" id="nome" placeholder="Nome do aluno" onChange={(e) => setSearch(e.target.value)}/>
-                        </div>
-                        <div>
-                            <label htmlFor="nacionalidade">Nacionalidade</label>
-                            <select 
-                                name="nacionalidade" 
-                                id="nacionalidade" 
-                                onChange={(e) => setSearchCountry(e.target.value)}
-                            
-                            >
-                                <option value="">Todas</option>
-                                {
-                                    dataFetching.map((e, index) => {
-                                        return (
-                                            <option key={index} value={e.location.country}>
-                                                {e.location.country}
-                                            </option>
+                            <FilterForm>
+                                <div>
+                                    <label htmlFor="nome">Pesquisar</label>
+                                    <input 
+                                        type="text" 
+                                        id="nome" 
+                                        placeholder='Nome do aluno'
+                                        onChange={(e) => setSearch(e.target.value)}
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="nacionalidade">Nacionalidade</label>
+                                    <select 
+                                        name="nacionalidade" 
+                                        id="nacionalidade"
+                                        onChange={(e) => setSearchCountry(e.target.value)}
+                                        value={searchCountry}
+                                    >
+                                        <option value="">Todas</option>
+                                        {
+                                            dataFetching.map((e, index) => {
+                                                return (
+                                                    <option key={index} value={e.location.country}>{e.location.country}</option>
+                                                )
+                                            })
+                                        }
+                                    </select>
+                                </div>
+                            </FilterForm>
+                            <TableContent>
+                                <thead>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Sexo</th>
+                                        <th>Nacionalidade</th>
+                                        <th>Ação</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        searchCountry.length > 0 || search.length > 0 ? (
+                                            actuallyFilter.map((e, index) => {
+                                                return (
+                                                    <tr key={index}>
+                                                        <td>{`${e.name.first} ${e.name.last}`}</td>
+                                                        <td>{e.gender}</td>
+                                                        <td>{e.location.country}</td>
+                                                        <td>
+                                                            <button onClick={() => handleOpenModalInfo(e)}>Visualizar</button>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })
+                                        ) : (
+                                            dataFetching.map((e, index) => {
+                                                return (
+                                                    <tr key={index}>
+                                                        <td>{`${e.name.first} ${e.name.last}`}</td>
+                                                        <td>{e.gender}</td>
+                                                        <td>{e.location.country}</td>
+                                                        <td>
+                                                            <button onClick={() => handleOpenModalInfo(e)}>Visualizar</button>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })
                                         )
-                                    })
-                                }
-                            </select>
-                        </div>
-                    </FilterForm>
-                    <TableContent>
-                        <thead>
-                            <tr>
-                                <th>Nome</th>
-                                <th>Sexo</th>
-                                <th>Nacionalidade</th>
-                                <th>Ação</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                dataFetching.map((e, index) => {
-                                    return(
-                                        <tr key={index}>
-                                            <td>{`${e.name.first} ${e.name.last}`}</td>
-                                            <td>{e.gender}</td>
-                                            <td>{e.nat}</td>
-                                            <td>
-                                                <button>Visualizar</button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </TableContent>
+                                    }
+                                </tbody>
+                            </TableContent>
                         </>
                     )
                 }
-
             </Content>
+            <ModalInfo 
+                isOpen={isModalInfoOpen}
+                onRequestClose={handleCloseModalInfo}
+                userSelected={dataUser}
+            />
         </Container>
     )
 }
